79483f82eb0262c51157cacd145cb4ac2fc7f2f8
BRYAN DE QUEIROZ FREITAS <48605781+bryanqf@users.noreply.github.com>
2022-06-03 22:45:19 -0300

    Update script.js
    Incrementando uma função para colocar o botão de reiniciar quando bate na palmeira..

--- JogoUrsoTropical/js/script.js
+++ JogoUrsoTropical/js/script.js
@@ -1,5 +1,7 @@
-const urso = document.querySelector('.urso')
-const palmeira = document.querySelector('.palmeira')
+const urso = document.querySelector('.urso');
+const palmeira = document.querySelector('.palmeira');
+const cipo = document.querySelector('.cipo');
+const board = document.querySelector('.board');
 function pulo(){
     urso.classList.add('pulo');
 
@@ -11,7 +13,10 @@ function pulo(){
 const verificaColisao = setInterval(()=>{
     const ursoPosicao = +window.getComputedStyle(urso).bottom.replace('px', '')
     const palmeiraPosicao = palmeira.offsetLeft;
+    const cipoPosicao = window.getComputedStyle(cipo).right.replace('px', '');
+
     if(palmeiraPosicao <= 59 && ursoPosicao < 210 && palmeiraPosicao > 0){
+        
         palmeira.style.animation = 'none';
         palmeira.style.left = `${palmeiraPosicao}px`
         urso.style.animation = 'none';
@@ -19,9 +24,26 @@ const verificaColisao = setInterval(()=>{
 
         urso.src='./img/acidente.png'
         urso.style.width = '100px'
-         clearInterval(verificaColisao);
+
+        
+        cipo.style.animation = 'none';
+        cipo.style.right = `${cipoPosicao}px`;
+
+        
+        
+        clearInterval(verificaColisao);
+        criarBotao('reiniciar')
     }
     
 })
 
+function criarBotao(texto){
+    const botao = document.createElement('button');
+    const text = document.createTextNode(texto);
+    botao.appendChild(text);
+    botao.onclick = ()=>{
+        document.location.reload(false);
+    }
+    document.body.appendChild(botao);
+}
 document.addEventListener('keydown', pulo)
